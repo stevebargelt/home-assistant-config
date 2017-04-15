eab01faabe20595bae1b5ebe24be955723e8fd18
Steve Bargelt <steve@bargelt.com>
2017-04-14 19:15:31 -0500

    Running Mosquitto MQQT in docker container

--- startha.sh
+++ startha.sh
@@ -1,2 +1,3 @@
+docker run -d --name="mqtt" -p 1883:1883 -p 9001:9001 -v /home/steve/homeassistant/mosquitto.conf:/mosquitto/config/mosquitto.conf eclipse-mosquitto
 docker run -d --name="home-assistant" -v /etc/letsencrypt:/etc/letsencrypt -v /home/steve/homeassistant:/config -v /etc/localtime:/etc/localtime:ro --device=/dev/ttyUSB0:/zwaveusbstick:rwm --net=host homeassistant/home-assistant:dev
 
