c6c452780e1aa2cccbce74f33be09605c8f93863
Steve Bargelt <steve@bargelt.com>
2017-05-01 16:16:12 -0500

    Fixed Polly Say mistake.

--- restartha.sh
+++
@@ -1,3 +0,0 @@
-docker stop home-assistant
-docker rm home-assistant
-docker run -d --name="home-assistant" -v /etc/letsencrypt:/etc/letsencrypt -v /home/steve/homeassistant:/config -v /etc/localtime:/etc/localtime:ro --device=/dev/ttyUSB0:/zwaveusbstick:rwm --net=host homeassistant/home-assistant:dev
