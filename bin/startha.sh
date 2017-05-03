8f7d38b335abcf20c5c981cc873f214f633eb5f7
Steve Bargelt <steve@bargelt.com>
2017-05-02 20:15:29 -0500

    Removing DEV from HA container

--- bin/startha.sh
+++ bin/startha.sh
@@ -1,5 +1,5 @@
 
 docker run -d --name="mqtt" -p 1883:1883 -p 9001:9001 -v /home/steve/homeassistant/mosquitto.conf:/mosquitto/config/mosquitto.conf 
 -v /mosquitto/data -v /mosquitto/log eclipse-mosquitto
-docker run -d --name="home-assistant" -v /etc/letsencrypt:/etc/letsencrypt -v /home/steve/homeassistant:/config -v /etc/localtime:/etc/localtime:ro --device=/dev/ttyUSB0:/zwaveusbstick:rwm --net=host homeassistant/home-assistant:dev
+docker run -d --name="home-assistant" -v /etc/letsencrypt:/etc/letsencrypt -v /home/steve/homeassistant:/config -v /etc/localtime:/etc/localtime:ro --device=/dev/ttyUSB0:/zwaveusbstick:rwm --net=host homeassistant/home-assistant
 
