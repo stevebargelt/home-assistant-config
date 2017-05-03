1b0700cbed7f5bdec6c5556a014a1f3b23adef0b
Steve Bargelt <steve@bargelt.com>
2017-05-03 15:42:02 -0500

    Securing MQTT, adding owntracks, removing locative

--- bin/startha.sh
+++ bin/startha.sh
@@ -1,5 +1,6 @@
 
-docker run -d --name="mqtt" -p 1883:1883 -p 9001:9001 -v /home/steve/homeassistant/mosquitto.conf:/mosquitto/config/mosquitto.conf 
--v /mosquitto/data -v /mosquitto/log eclipse-mosquitto
+docker run -d --name="mqtt" -p 8883:8883 -p 9001:9001 -v /etc/letsencrypt:/etc/letsencrypt -v /home/steve/homeassistant/mosquitto.conf:/mosquitto/config/mosquitto.conf
+-v /home/steve/homeassistant/mosquitto/data:/mosquitto/data -v /home/steve/homeassistant/log:/mosquitto/log eclipse-mosquitto
+
 docker run -d --name="home-assistant" -v /etc/letsencrypt:/etc/letsencrypt -v /home/steve/homeassistant:/config -v /etc/localtime:/etc/localtime:ro --device=/dev/ttyUSB0:/zwaveusbstick:rwm --net=host homeassistant/home-assistant
 
