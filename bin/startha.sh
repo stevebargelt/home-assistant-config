129ff2768c7b0889ac01c50919f49e8b920187af
Steve Bargelt <steve@bargelt.com>
2017-05-03 16:06:01 -0500

    Wrong log path

--- bin/startha.sh
+++ bin/startha.sh
@@ -1,5 +1,5 @@
 
-docker run -d --name="mqtt" -p 8883:8883 -p 9001:9001 -v /etc/letsencrypt:/etc/letsencrypt -v /home/steve/homeassistant/mosquitto.conf:/mosquitto/config/mosquitto.conf -v /home/steve/homeassistant/mosquitto/data:/mosquitto/data -v /home/steve/homeassistant/log:/mosquitto/log eclipse-mosquitto
+docker run -d --name="mqtt" -p 8883:8883 -p 9001:9001 -v /etc/letsencrypt:/etc/letsencrypt -v /home/steve/homeassistant/mosquitto.conf:/mosquitto/config/mosquitto.conf -v /home/steve/homeassistant/mosquitto/data:/mosquitto/data -v /home/steve/homeassistant/mosquitto/log:/mosquitto/log eclipse-mosquitto
 
 docker run -d --name="home-assistant" -v /etc/letsencrypt:/etc/letsencrypt -v /home/steve/homeassistant:/config -v /etc/localtime:/etc/localtime:ro --device=/dev/ttyUSB0:/zwaveusbstick:rwm --net=host homeassistant/home-assistant
 
